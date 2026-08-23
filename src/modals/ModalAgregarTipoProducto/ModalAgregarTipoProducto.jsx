3d2f46a3f203f8e7cf09c2dad64d55e0489e26ea
ignjor <ignaciojorqueradiaz.ij@gmail.com>
2026-08-23 15:40:14 -0400

    Vamos en el proceso de ModalAgregarProducto pero con las medidas_asig en un solo modal

--- src/modals/ModalAgregarTipoProducto/ModalAgregarTipoProducto.jsx
+++ src/modals/ModalAgregarTipoProducto/ModalAgregarTipoProducto.jsx
@@ -2,23 +2,27 @@ import { useEffect, useRef, useState } from "react";
 import "./ModalAgregarTipoProducto.css";
 
 import { db } from "../../firebase";
+import { addDoc, collection, updateDoc, doc } from "firebase/firestore";
 
-import { addDoc, collection, updateDoc, doc, getDoc } from "firebase/firestore";
+import {CircleX, CirclePlus, X, Plus, Trash2, IndianRupee
+} from "lucide-react";
 
-import {CircleX, CirclePlus, X} from "lucide-react";
-
-
-export default function ModalAgregarTipoProducto({ abierto, onCerrar, onTipoPrendaAgregado, tipoPrenda }) {
+/* Creamos la funcion del modal, que va a tener todas las funciones, y lo hacemos llamando a las variables y estados que 
+traemos desde TipoPrenda que es donde se usa este modal principalmente */
+export default function ModalAgregarTipoProducto({abierto, onCerrar, onTipoPrendaAgregado, tipoPrenda}) {
     const [nombreTipoPrenda, setNombreTipoPrenda] = useState("");
+    const [medidasAsignadas, setMedidasAsignadas] = useState([]); /*Esta de las medidas es una lista porque cuando creamos, no sabemos cuantos va acrear, solo sabemos que maximo 10 */
+
     const [error, setError] = useState("");
     const [guardando, setGuardando] = useState(false);
+    
+    const modalRef = useRef(null);
 
-    /* Creamos esta funcion para validad los inputs, en nuestro caso este caso es solo el nombre del colegio, para limitar
-    los nombres especiales y tambien limitar las inserciones de codigo, pero obvio esto no es seguridad, porque todo lo del front
-    end se puede vulnerar facil desde el navegador con un devtool, por eso tambien colocamos reglas de firestore seguras para evitar
-    insercioens de codigo. */ 
-    const validadNombre = (tipoPrenda) => {
-        const nombreLimpio = tipoPrenda.trim();
+    /* Validamos nombre, bueno no nombre si no los imputs, se llama nombre por que la cree en colegios y lo que funciona no se cambia
+    La usamos para validar los inputs dentro del Tipo y las medidas asig. */
+    const validarNombre = (nombre) => {
+
+        const nombreLimpio = nombre.trim();
 
         if (nombreLimpio.length === 0) {
             return "Debes ingresar el nombre.";
@@ -42,199 +46,76 @@ export default function ModalAgregarTipoProducto({ abierto, onCerrar, onTipoPren
         return null;
     };
 
-    /* Aqui si finalmente agregamos al colegio para guardarlo desde obvio la primera validaciom */
+
+
+    /* Esta funcion agrega atributo, es simplemente para ver cuantos hay, si hay menos de 10, que lo agregue nomas*/
+    const agregarAtributo = () => {
+        if (medidasAsignadas.length >= 10){
+            return;
+        }
+        setMedidasAsignadas([
+            ...medidasAsignadas,""
+        ]);
+    };
+
+    /* Aqui cambiamos atributo, recibimos el index del medidasasignadas, la opciocon del array, com oes un array empieza de 0 pa lante.*/
+    const cambiarAtributo = (index, valor) => {
+        setMedidasAsignadas((atributosActuales) => {
+            const nuevosAtributos = [...atributosActuales];
+            nuevosAtributos[index] = valor;
+            return nuevosAtributos;
+        });
+        setError("");
+    }
+
+    /* ahora aqui usamos esta funcion para eliminar el atributo necesario dentor del array con filter, asi sin arrastrar lo otro jasjda, y no haciendolo antes de que apretemos borrar */
+    const eliminarAtributo = (index) => {
+        setMedidasAsignadas((atributosActuales) =>
+            atributosActuales.filter(
+                (_, indice) => indice !== index
+            ));
+        setError
+    }
+
+
+
+    /* Aqui guardamos el tipo, es decir el nombre, es practicamente en funcion identica a la del modal de colegios
+    en el nombre. AGREGAmOS eso si, lo de las medidas, y aplicamos lo mismo par a el input, el tema del nombre limpio con nuestras
+    restricciones y varchar, que en realidad como es frontend no restringe nadajs dja, porque las reales restricciones estan dentro de 
+    firestore rules*/
     const agregarTipoPrenda = async () => {
         const nombreLimpio = nombreTipoPrenda.trim();
-        const errorValidacion = validadNombre(nombreLimpio);
+        const errorValidacion =
+            validarNombre(nombreLimpio);
+
         if (errorValidacion) {
             setError(errorValidacion);
             return;
         }
+
+        const atributosLimpios = medidasAsignadas
+            /* Este sabrongo, mira lo que hace, te convierte un array en un map despues del trim, entonces, te toma los 
+            espacios, los borra, y lo convierte en un array, una chulada papá. */ 
+            .map((atributo) => atributo.trim()) .filter((atributo) => atributo.length > 0);
+        for (const atributo of atributosLimpios) {
+            const errorAtributo =
+                validarNombre(atributo);
+            if (errorAtributo) {
+                setError("El atributo no es valido:",atributo);
+            } 
+            return;
+        }
+    /* esta parte es suuper importante, porque si, antes estabamos tratando a los atributos, a las medidas asig como un array, aqui lo
+    convertir a nuestro map que queremos en firestore para guardar todo ordenadito y bonito, que luego productos llamara, excelenteeeee */
+        const medidasAsig = {};
+        atributosLimpios.forEach((atributo) => {
+            medidasAsig[atributo] = "";
+        });
         try {
             setGuardando(true);
             setError("");
-
-            if (tipoPrenda) {
-                    await updateDoc(
-                    doc(db,"tipo_prenda", tipoPrenda.id),
-                    {
-                        tipo: nombreLimpio
-                    }
-                );
-            } else {
-                await addDoc(
-                    collection(db, "tipo_prenda"),
-                    {
-                        tipo: nombreLimpio
-                    }
-                );
-
-            }
-            await onTipoPrendaAgregado()
-            setNombreTipoPrenda("");
-            onCerrar();
-        } catch (error) {
-            console.error("Error al agregar a la base de datos:", error);
-            setError("No se pudo guardar, intente denuevo.");
-        } finally {
-            setGuardando(false);
         }
-    };
 
 
-    const modalRef = useRef(null);
 
-    useEffect(() => {
-        if (!abierto) {
-            return;
-        }
-        const clickFuera = (event) => {
-            if (
-                modalRef.current &&
-                !modalRef.current.contains(event.target)
-            ) {
-                onCerrar();
-            }
-        };
-        document.addEventListener("mousedown", clickFuera);
-        return () => {
-            document.removeEventListener(
-                "mousedown",
-                clickFuera
-            );
-        };
-    }, [abierto, onCerrar]);
-
-    useEffect(() => {
-        if (!abierto) {
-            setNombreTipoPrenda("");
-            setError("");
-            return;
-        }
-        if (tipoPrenda) {
-            setNombreTipoPrenda(tipoPrenda.tipo);
-        } else {
-            setNombreTipoPrenda("");
-        }
-        setError("");
-    }, [abierto, tipoPrenda])
-    
-
-
-  // Si el modal no está abierto, no renderizamos nada.
-    if (!abierto) {
-        return null;
-    }
-
-    return (
-
-        <div className="modalColegioOverlay">
-
-        <div
-            className="modalColegio"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="modalColegioTitle"
-            ref = {modalRef}
-        >
-
-            {/* Encabezado del modal */}
-            <header className="modalColegioHeader">
-
-            <h2
-                id="modalColegioTitle"
-                className="modalColegioTitle"
-            >
-                {tipoPrenda
-                ? "Editar tipo de Prenda / Producto"
-                : "Agregar tipo de Prenda / Producto"
-                }
-            </h2>
-
-            <button
-                type="button"
-                className="modalColegioClose"
-                onClick={onCerrar}
-                aria-label="Cerrar"
-                disabled={guardando}
-            >
-                <X size={17} strokeWidth={2} />
-            </button>
-
-            </header>
-
-
-            {/* Contenido principal */}
-            <div className="modalColegioContent">
-
-            <div className="modalColegioField">
-
-                <label
-                htmlFor="nombreColegio"
-                className="modalColegioLabel"
-                >
-                Escribe el nombre del tipo de Prenda / Producto
-                </label>
-
-                <input
-                id="nombreColegio"
-                type="text"
-                className="modalColegioInput"
-                placeholder="Ej. Pantalón, Vestido"
-                value={nombreTipoPrenda}
-                onChange={(event) => {
-                    setNombreTipoPrenda(event.target.value);
-                    if (error) {
-                        setError("");
-                    }
-                }}
-                maxLength={64}
-                autoComplete="off"
-
-                disabled={guardando}
-                />
-                {error && (
-                <p className="modalColegioError">
-                    {error}
-                </p>
-                )}
-            </div>
-
-            </div>
-
-
-            {/* Acciones */}
-            <footer className="modalColegioActions">
-            
-            <button
-                type="button"
-                className="modalColegioButton modalColegioButtonCancel"
-                onClick={onCerrar}
-                disabled={guardando}
-            >
-                <CircleX size={17} strokeWidth={2} />
-                <span>
-                    Cancelar
-                </span>
-            </button>
-
-            <button
-                type="submit"
-                className="modalColegioButton modalColegioButtonPrimary"
-                disabled={guardando}
-                onClick={agregarTipoPrenda}
-            >
-                <CirclePlus size={17} strokeWidth={2} />
-                <span>
-                    {guardando ? "Guardando..." : "Agregar"}
-                </span>
-            </button>
-
-            </footer>
-
-        </div>
-
-        </div>
-
-    );
-    }
+}
