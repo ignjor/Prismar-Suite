c0b24c2fdeaadb4116e3ef40500523bce0ff79ad
ignjor <ignaciojorqueradiaz.ij@gmail.com>
2026-08-25 21:51:37 -0400

    refactor: Refactorizamos BottomNav completo, optimizamos estructura

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,190 +1,83 @@
 import { NavLink } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
-
 import "./BottomNav.css";
 
 import { Home, ShoppingBag, Package, PiggyBank, Plus, School, Baby, BanknoteArrowDown, Shirt } from "lucide-react";
 
-/* 
+const rutasDeNavegacion = [
+    {name: "Inicio", path: "/", icon: Home},
+    {name: "Pedidos", path: "/pedidos", icon: ShoppingBag},
+    {name: "Productos", path: "/productos", icon: Package},
+    {name: "Cuentas", path: "/cuentas", icon: PiggyBank},
+];
+const rutasDeNavegacionBotonPlus = [
+    {name: "Agregar Pedido", path: "/agregar-pedido", icon: ShoppingBag},
+    {name: "Agregar Producto", path: "/agregar-producto", icon: Package},
+    {name: "Agregar Gasto", path: "/agregar-gasto", icon: BanknoteArrowDown},
+    {name: "Empresas", path: "/admin-colegios", icon: School},
+    {name: "Tipos de Prenda", path: "/tipo-prenda", icon: Shirt},
+];
 
-Todas las funciones del código tienen que ser comentadas, para su
-correcto funcionamiento, aprendizaje y debuggeado en caso de cualquier
-error.
-
-*/ 
 
 export default function BottomNav() {
+    const [estadoBotonPlus, setEstadoBotonPlus] = useState(false);
+    const estadoBotonPlusRef = useRef(null);
+    const cambiarEstadoBotonPlus = () => setEstadoBotonPlus(estado => !estado);
 
-
-    /* Variable para guardar el estado del boton y saber cuando esta abierto, predeterminado esta cerrado, por eso 
-    eso el state en false */
-    const [mostrarBoton, setMostrarBoton] = useState(false);
-    const botonRef = useRef(null);
-
-    /* Definimos las variables de las direcciones de la barra
-    de navegación, con sus nombres para reconocerlas, les asignamos
-    direcciones a cada una, Las direcciones path estan dentro de App.jsx por si
-    en el futuro se agregan mas páginas. */
-    const direcciones = [
-
-        {
-            name: "Inicio",
-            path: "/",
-            icon: Home
-        },
-        {
-            name: "Pedidos",
-            path: "/pedidos",
-            icon: ShoppingBag
-        },
-        {
-            name: "Productos",
-            path: "/productos",
-            icon: Package
-        },
-        {
-            name: "Cuentas",
-            path: "/cuentas",
-            icon: PiggyBank
-        },
-
-
-    ];
-
-
-    /* Definimos las variables de las direcciones que se van a mostrar en el boton verde. */
-    const agregar = [
-        {
-            name: "Agregar Pedido",
-            path: "/agregar-pedido",
-            icon: ShoppingBag
-        },
-        {
-            name: "Agregar Producto",
-            path: "/agregar-producto",
-            icon: Package
-        },
-        {
-            name: "Agregar Gasto",
-            path: "/agregar-gasto",
-            icon: BanknoteArrowDown
-
-        },
-        {
-            name: "Empresas",
-            path: "/admin-colegios",
-            icon: School
-        },
-        {
-            name: "Tipos de Prenda",
-            path: "/tipo-prenda",
-            icon: Shirt
-        },
-        {
-            name: "Gonguru",
-            path: "/gonguru",
-            icon: Baby
-        }
-
-        
-    ];
-
-    /* Creamos la fucion para cambiar el estado del boton, la vamos a llamar cuando apretemos el boton */
-    const cambiarEstadoBoton = () => {
-        setMostrarBoton((estadoActual) => !estadoActual);
-    };
-
-
-
-    /* Funcion para escuchar donde clickea SOLO cuando el menu esta afuera */
+    
     useEffect(() => {
+        if (!estadoBotonPlus) return;
         
-        /* Si el menu del boton esta cerrado no tenemos porque estar escuchando el click, se ignora la funcion con este IF */
-        if (!mostrarBoton) {
-            return;
-        }
-
-        const clickFuera = (event) => {
-            if (botonRef.current && !botonRef.current.contains(event.target)) {
-                setMostrarBoton(false);
+        const clickFueraBotonPlus = (event) => { 
+            if (estadoBotonPlusRef.current && !estadoBotonPlusRef.current.contains(event.target)){
+                setEstadoBotonPlus(false);
             }
         };
+        document.addEventListener("mousedown", clickFueraBotonPlus);
 
-        document.addEventListener("mousedown", clickFuera);
+        return () => {document.removeEventListener("mousedown", clickFueraBotonPlus)};
+    }, [estadoBotonPlus]);
 
-        return () => {
-            document.removeEventListener("mousedown", clickFuera);
-        };
-    }, [mostrarBoton]);
 
     return (
-            
         <div className="bottomContainer"> 
-
-
-
             <nav className="bottomNav">
-
-            {/* Mapeamos el bloque de las variables de las direcciones
-            para tenerlas todas, creamos la variable temporal de direccion
-            para asignar la que esta activa en ese momento */}
-
-                {direcciones.map((direccion) => {
-
-                    const Icon = direccion.icon;
-
+                {rutasDeNavegacion.map((ruta) => {
+                    const Icon = ruta.icon;
                     return (
                         <NavLink
-                        key = { direccion.path } 
-                        to = {direccion.path}
-
-                        /* Ademas asignamos que la ventan que esta activa en ese momento
-                        le asignamos un estilo especial a la barra */
-
+                        key = { ruta.path } 
+                        to = {ruta.path}
                         className = {({isActive}) => 
                             `navItem ${isActive ? "active" : ""}`
-                        }
-                        
+                        } 
                         >
-
                             <Icon size = {23}/>
-
-                            <span> {direccion.name} </span>
-                            
+                            <span> {ruta.name} </span>
                         </NavLink>
                     );
-                })
-                    
-                }
-
+                })}
             </nav>
             
-            <div ref = {botonRef} className="addContainer">    
-            
+            <div ref = {estadoBotonPlusRef} className="addContainer">    
                 <div 
                     className="addMenu"
                     style={{
-                        opacity: mostrarBoton ? 1 : 0,
-                        transform: mostrarBoton ? "translateY(0) scale(1)" : "translateY(10px) scale(0.95)",
-                        pointerEvents: mostrarBoton ? "auto" : "none"
+                        opacity: estadoBotonPlus ? 1 : 0,
+                        transform: estadoBotonPlus ? "translateY(0) scale(1)" : "translateY(10px) scale(0.95)",
+                        pointerEvents: estadoBotonPlus ? "auto" : "none"
                     }}
                 >
-
-                    {agregar.map((opcion) => {
-                        
+                    {rutasDeNavegacionBotonPlus.map((opcion) => {
                         const Icon = opcion.icon;
-
                         return (
                             <NavLink
                                 key = {opcion.path}
                                 to = {opcion.path}
-
-                                onClick = {() => setMostrarBoton(false)}
+                                onClick = {() => setEstadoBotonPlus(false)}
                                 className="addOption"
                             >
-
                             <Icon size = {18}/>
-
                             <span> {opcion.name} </span>
                             </NavLink>
                         )
@@ -197,23 +90,38 @@ export default function BottomNav() {
 
                 <button
                     type = "button"
-                    onClick = {cambiarEstadoBoton}
+                    onClick = {cambiarEstadoBotonPlus}
                     className="addButton"
                     aria-label = "Agregar"
-                    aria-expanded = {mostrarBoton}
+                    aria-expanded = {estadoBotonPlus}
                 >
                     <Plus
                         size = {24}
                         strokeWidth = {2.3}
                         style = {{
-                            transform: mostrarBoton ? "rotate(45deg)" : "rotate(0deg)",
+                            transform: estadoBotonPlus ? "rotate(45deg)" : "rotate(0deg)",
                             transition: "transform .2s ease"
                         }}
                     /> 
-
                 </button>  
             </div>
         </div>
-
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
