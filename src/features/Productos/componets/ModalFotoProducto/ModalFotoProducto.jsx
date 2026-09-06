bdfb5a9f0592ac4cc1f2d35e532b536fb4faa826
ignjor <ignaciojorqueradiaz.ij@gmail.com>
2026-09-05 22:05:26 -0400

    style: limpia el .css de botomnav, le baja el index a 900 para que los modals cuando abran tengan protagonismo y el boton no los tape

--- src/features/Productos/componets/ModalFotoProducto/ModalFotoProducto.jsx
+++ src/features/Productos/componets/ModalFotoProducto/ModalFotoProducto.jsx
@@ -1,3 +1,10 @@
-export default function ModalFotoProducto() {
+import { useEffect, useRef, useState } from "react";
+import "./ModalFotoProducto.css";
 
-};
+import { db, storage } from "../../../../firebase";
+import { updateDoc, doc} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+import { Camera, Check, ImagePlus, LoaderCircle, Upload, CircleX } from "lucide-react";
+
+export default function ModalFotoProducto({ producto, modalAbierto, onCerrarModal }) {}
